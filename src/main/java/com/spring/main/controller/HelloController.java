51f211bd97fb25cd9dcd64e915c56175095fb31c
arbazadam <42610433+arbazadam@users.noreply.github.com>
2020-09-19 15:43:30 +0400

    first commit

---
+++ src/main/java/com/spring/main/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.spring.main.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+@GetMapping("/hello")
+public String getHello()
+{
+return "template server side app";	
+}
+}
