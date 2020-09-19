5afc7c5bcc051513b90f00ff462e2a5ea8ea9a2d
arbazadam <42610433+arbazadam@users.noreply.github.com>
2020-09-19 19:31:39 +0400

    adding a noBranch method

--- src/main/java/com/spring/main/controller/HelloController.java
+++ src/main/java/com/spring/main/controller/HelloController.java
@@ -1,10 +1,7 @@
 package com.spring.main.controller;
 
-import java.io.File;
 import java.util.Map;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -22,5 +19,10 @@ return "template server side app";
 public void postData(@RequestHeader Map<String, String> headers)
 {
 	System.out.println(Thread.currentThread().getName());
+}
+@PostMapping("/valueless")
+public void noBranch()
+{
+	System.out.println("Hello MOJO");
 }
 }
